ca6315b9c774dae9ae5d6a32af137c536318bc56
Diane Hoffstetter <dianehof@seas.upenn.edu>
2022-03-25 17:00:44 -0700

    Initial commit

---
+++ resources/scripts/libnss_wrapper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+
+echo "Install nss-wrapper to be able to execute image as non-root user"
+apt-get update 
+apt-get install -y libnss-wrapper gettext
+apt-get clean -y
+
+echo "add 'source generate_container_user' to .bashrc"
+
+# have to be added to hold all env vars correctly
+echo 'export HOME="/home/cis553"' >> $HOME/.bashrc
+#echo 'source /home/cis553/.profile' >> $HOME/.bashrc
